227353802fd43b7f6ff1977854d56d3118aab711
juhy-git <libor.juhanak@gmail.com>
2017-03-22 19:55:52 +0100

    general sql

--- inst/sql/general/get_course_modules.sql
+++ inst/sql/general/get_course_modules.sql
@@ -4,8 +4,9 @@ SELECT
   s.section AS 'module.section',
   s.name AS 'section.name',
   m.name AS 'module.type',
-  cm.id AS 'module.id',              # = cmid
-  cm.instance AS 'module.instance',  # = id --- asi není třeba mít mít cmid i id, nebo?
+  cm.id AS 'module.id',              # = cmid - používá se pak v dotazech k jednotlivým modulů (nyní quiz) 
+  cm.instance AS 'module.instance',  # = id - přes id se napojuje na názvy modulů 
+                                     # (teoreticky by pak šlo všude pracovat jen s id, cmid ale může být praktický v tom, že ho pak vidím v URL)
   
   # celý následující SELECT zřejmě samzat a místo toho opakovaně volat upravený get_modules_names
   # a nasledně to ponapojovat na výstup tohoto dotazu přes id modulů --- nebo je lepší řešení?
