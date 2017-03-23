49457f236c77e2cacfe8b94dfe8ddacbdffd90a4
Jakub Chromec <chromec.jakub@gmail.com>
2017-03-23 10:31:24 +0100

    add [prefix] field

--- inst/sql/general/get_courses.sql
+++ inst/sql/general/get_courses.sql
@@ -1,4 +1,4 @@
-SELECT 
+SELECT
 
   c.id AS 'course.id',
   c.shortname AS 'course.shortname',
@@ -6,11 +6,11 @@ SELECT
   c.visible AS 'course.visible',
   FROM_UNIXTIME(c.timecreated) AS 'course.timecreated',
   FROM_UNIXTIME(c.timemodified) AS 'course.timemodified',
-  (SELECT cc.id FROM mdl_course_categories AS cc 
+  (SELECT cc.id FROM mdl_course_categories AS cc
 		WHERE cc.id = c.category) AS 'course.categoryid',
-  (SELECT cc.name FROM mdl_course_categories AS cc 
+  (SELECT cc.name FROM mdl_course_categories AS cc
 		WHERE cc.id = c.category) AS 'course.categoryname'
         # Napadlo mně, jestli zde třeba nemůže být užitečná i kategorie, kdyžtak lze vyhodit...
 
-FROM mdl_course AS c;
+FROM [prefix]course AS c;
 
