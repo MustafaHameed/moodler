572c10d67d0b4a96cb13eb1eba473192dbec56de
juhy-git <libor.juhanak@gmail.com>
2017-03-24 22:55:44 +0100

    add gapselect

---
+++ inst/sql/quiz/get_gapselect_key.sql
@@ -0,0 +1,41 @@
+SELECT
+
+  -- Quiz related
+  q.course AS 'course.id',
+  cm.id AS 'quiz.id',
+  
+  -- Question related  
+  que.qtype AS 'question.type',
+  quea.questionid AS 'question.id',
+  COUNT(quea.questionid) AS 'question.attempts',   # toto k něčemu potřebujeme?
+  que.name AS 'question.name',
+  que.questiontext AS 'question.text',
+  qs.maxmark AS 'question.maxpoints',
+  quea.minfraction AS 'question.mingrade',
+  quea.rightanswer AS 'right.answers',
+  
+  -- Answer related   
+  qans.id AS 'response.id',   
+  qans.answer AS 'response.text',
+  qans.feedback AS 'response.group'
+
+FROM [prefix]quiz AS q
+JOIN [prefix]course_modules AS cm
+  ON q.course = cm.course AND q.id = cm.instance
+JOIN [prefix]quiz_attempts AS qa
+  ON q.id = qa.quiz
+JOIN [prefix]question_attempts AS quea
+  ON qa.uniqueid = quea.questionusageid
+JOIN [prefix]question AS que
+  ON quea.questionid = que.id
+JOIN [prefix]quiz_slots AS qs
+  ON q.id = qs.quizid AND que.id = qs.questionid  
+LEFT JOIN [prefix]question_answers AS qans
+  ON quea.questionid = qans.question
+
+WHERE que.qtype = 'gapselect' AND 
+      cm.id IN ([module.id])
+
+GROUP BY q.id, quea.questionid, qans.id
+
+ORDER BY q.id, quea.questionid;
