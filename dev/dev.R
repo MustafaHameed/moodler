70c7f8c738288d08edf8ff669399d01d20fb6055
Jakub Chromec <chromec.jakub@gmail.com>
2017-03-25 10:29:22 +0100

    more testing quizes

--- dev/dev.R
+++ dev/dev.R
@@ -5,6 +5,10 @@ library(dplyr)
 
 .con = dbConnect(RMySQL::MySQL(), group = "moodler")
 dbGetQuery(.con, "SET NAMES utf8")
+dbGetQuery(.con, "SET sql_mode = ''")
 
 q74 = get_quiz(.con, 74)
-get_truefalse(q74)
+tf = moodler:::get_truefalse(q74, attempt.id = c(2514, 2533))
+
+q79 = get_quiz(.con, 79)
+sa = moodler:::get_shortanswer(q79, attempt.id = 2534:2435)
