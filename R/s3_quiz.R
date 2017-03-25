a17ad7af72b88f3bf0d25d0bea60270644abcb43
Jakub Chromec <chromec.jakub@gmail.com>
2017-03-25 14:24:54 +0100

    add data fetching s3

--- R/s3_quiz.R
+++ R/s3_quiz.R
@@ -46,5 +46,66 @@ print.mdl_quiz = function(x, ...) {
     as.data.frame(table(x$questions$question.type)),
     names = c("item.type", "count"))
 
-  print(item_count)
+  print(item_count, ...)
+}
+
+#' Get Quiz Data
+#'
+#' Fetch item data from quiz.
+#' @param x An object of class \code{"mdl_quiz"}
+#' @param attempt Defaults to \code{"first"} but \code{"all"} and \code{"last"} are also allowed
+#' @param question.type Char vector, if \code{NULL}, all items regardless of type will be fetched
+#' @param prefix Defaults to \code{"mdl_"}
+#' @param ... Further arguments passed on to methods
+#' @export
+
+get_module_data.mdl_quiz = function(x, attempt = "first",
+                                    question.type = NULL,
+                                    prefix = "mdl_",
+                                    ...) {
+
+  attempt_id = get_attempt_id(x = x$attempts, attempt = attempt)
+  available_types = unique(x$questions$question.type)
+
+  if (!is.null(question.type)) {
+    if (!all(question.type %in% available_types)) {
+      missing_type = paste(
+        question.type[!question.type %in% available_types],
+        collapse = ", ")
+      stop(missing_type, " not available in this quiz", call. = FALSE)
+    }
+  }
+
+  dat = lapply(available_types, function(this_type) {
+    fun_name = paste0("get_", this_type)
+    c(tryCatch(
+      expr = do.call(
+        what = fun_name,
+        args = list(conn = x$connection,
+                    quiz.id = x$settings$quiz.id,
+                    attempt.id = attempt_id,
+                    prefix = prefix)),
+      error = function(e)
+        message(e$message)
+      ))
+  })
+
+  structure(dat, names = available_types)
+}
+
+get_attempt_id = function(x, attempt = "first") {
+
+  stopifnot(attempt %in% c("first", "all", "last"))
+
+  attempt_number = list(
+    first = "attempt.number == min(attempt.number)",
+    all = "is.integer(attempt.number)",
+    last = "attempt.number == max(attempt.number)")
+
+  attempt = attempt_number[[attempt]]
+
+  x %>%
+    group_by(u.id) %>%
+    filter_(.dots = attempt) %>%
+    `[[`("attempt.id")
 }
--- R/s3_utils.R
+++ R/s3_utils.R
@@ -8,3 +8,13 @@
 #' @rdname is.mdl_
 is.mdl_quiz = function(x)
   inherits(x, "mdl_quiz")
+
+#' Get module data
+#'
+#' Fetch data for a specific module.
+#' @param x An object of class \code{"mdl_"}, e.g. \code{"mdl_quiz"}
+#' @param ... Further arguments passed on to methods
+#' @export
+
+get_module_data = function(x, ...)
+  UseMethod("get_module_data")
