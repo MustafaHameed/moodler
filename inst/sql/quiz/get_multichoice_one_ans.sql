1183d825dcbc985932cbeea95dccc5cffdc9e797
Jakub Chromec <chromec.jakub@gmail.com>
2017-03-27 17:43:41 +0200

    rename users.answer->answer.id; remove user.id

--- inst/sql/quiz/get_multichoice_one_ans.sql
+++ inst/sql/quiz/get_multichoice_one_ans.sql
@@ -3,17 +3,17 @@ SELECT
   -- Quiz/User/Attempt
   q.course AS 'course.id',
   cm.id AS 'quiz.id',
-  quiza.userid AS 'user.id',
+  #quiza.userid AS 'user.id', #toto si rikam ze je zbytecna duplikace sloupce v attempts
   quiza.id AS 'attempt.id',
-  
-  -- Question related  
+
+  -- Question related
   'multichoice_one' AS 'question.type',       # Zbytečné? Příp. rychlejší přes R?
   que.id AS 'question.id',
   quea.maxmark AS 'question.maxpoints.past',  # Je to třeba?
-  
+
   -- Answer related
   queasd.name AS 'answer.data',
-  queasd.VALUE AS 'users.answer',
+  queasd.VALUE AS 'answer.id',
   FROM_UNIXTIME(queas.timecreated) AS 'answer.time'
 
 FROM [prefix]quiz AS q
@@ -25,8 +25,8 @@ JOIN [prefix]question_attempts AS quea
   ON quea.questionusageid = quiza.uniqueid
 JOIN [prefix]question AS que
   ON quea.questionid = que.id
-JOIN [prefix]qtype_multichoice_options AS qmu 
-  ON qmu.questionid = que.id  
+JOIN [prefix]qtype_multichoice_options AS qmu
+  ON qmu.questionid = que.id
 JOIN [prefix]question_attempt_steps AS queas
   ON queas.questionattemptid = quea.id
 LEFT JOIN [prefix]question_attempt_step_data AS queasd
