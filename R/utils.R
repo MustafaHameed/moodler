76860d032caeb2f9d81b75338d1a9662c365ecb5
Jakub Chromec <chromec.jakub@gmail.com>
2017-03-27 18:30:44 +0200

    add count_commas

--- R/utils.R
+++ R/utils.R
@@ -9,3 +9,12 @@ reg_match = function(string, pattern) {
   match_text = regmatches(string, match_position)
   unlist(match_text)
 }
+
+# Return comma-separated cummulative count of comma-separated items
+# i.e. tre,tru,tri will return 1,2,3
+count_commas = function(x) {
+  paste(
+    0:length(reg_match(x, ",+")) + 1,
+    collapse = ","
+  )
+}
