0f4e9ae2e4251acbf863fe87e62d66fa8c74cf66
juhy-git <libor.juhanak@gmail.com>
2017-03-30 14:15:18 +0200

    change mutlianswer key structure

--- inst/sql/quiz/get_multianswer_key.sql
+++ inst/sql/quiz/get_multianswer_key.sql
@@ -1,38 +1,19 @@
 SELECT
 
-  -- Quiz related
-  q.course AS 'course.id',
-  cm.id AS 'quiz.id',
-
   -- Question related
-  que.qtype AS 'question.type',
-  quea.questionid AS 'question.id',
-  COUNT(quea.questionid) AS 'question.attempts',   # toto k něčemu potřebujeme?
+  que.id AS 'question.id',
+  que.qtype AS 'question.type',  
   que.name AS 'question.name',
   que.questiontext AS 'question.text',
-  qs.maxmark AS 'question.maxpoints',
-  quea.minfraction AS 'question.mingrade',
 
   -- Subquestions related
   quemulti.sequence AS 'subquestions.ids'
 
-FROM [prefix]quiz AS q
-JOIN [prefix]course_modules AS cm
-  ON q.course = cm.course AND q.id = cm.instance
-JOIN [prefix]quiz_attempts AS qa
-  ON q.id = qa.quiz
-JOIN [prefix]question_attempts AS quea
-  ON qa.uniqueid = quea.questionusageid
-JOIN [prefix]question AS que
-  ON quea.questionid = que.id
+FROM [prefix]question AS que
 JOIN [prefix]question_multianswer AS quemulti
   ON quemulti.question = que.id 
-JOIN [prefix]quiz_slots AS qs
-  ON q.id = qs.quizid AND que.id = qs.questionid
 
 WHERE que.qtype = 'multianswer' AND
-      cm.id IN ([module.id])
+      que.id IN ([question.id])
 
-GROUP BY q.id, quea.questionid
-
-ORDER BY q.id, quea.questionid;
+ORDER BY que.id, qans.id;
