3a87cf1b3eee26b523c52f2990d490bcdbd40e96
Jakub Chromec <chromec.jakub@gmail.com>
2017-03-30 15:30:35 +0200

    streamline code

--- R/get_mc.R
+++ R/get_mc.R
@@ -2,23 +2,16 @@
 #'
 #' Get multichoice (one naswer) item data.
 #' @param conn Connection object
-#' @param quiz.id Numeric vector of length 1
 #' @param attempt.id Vector of attempt ids
 #' @param prefix Defaults to \code{"mdl_quiz"}
 #' @param suppress.warnings Should warnings produced by \code{\link[DBI]{dbGetQuery}} be suppressed? Defaults to \code{TRUE}
 #' @importFrom dplyr %>% left_join select mutate
 #' @importFrom tidyr separate_rows
 
-get_multichoice_one = function(conn, quiz.id, attempt.id, prefix = "mdl_",
+get_multichoice_one = function(conn, attempt.id, prefix = "mdl_",
                                suppress.warnings = TRUE) {
 
-  key = get_question_key(
-    conn = conn,
-    question.type = "multichoice_one",
-    quiz.id = quiz.id,
-    prefix = prefix,
-    suppress.warnings = suppress.warnings)
-
+  # SQL queries
   ans = get_question_ans(
     conn = conn,
     question.type = "multichoice_one",
@@ -26,29 +19,48 @@ get_multichoice_one = function(conn, quiz.id, attempt.id, prefix = "mdl_",
     prefix = prefix,
     suppress.warnings = suppress.warnings)
 
-  ans_answer = ans %>%
+  key = get_question_key(
+    conn = conn,
+    question.type = "multichoice_one",
+    question.id = unique(ans$question.id),
+    prefix = prefix,
+    suppress.warnings = suppress.warnings)
+
+  # Determine selected
+  ans_given = ans %>%
     filter_latest(answer.data = "answer") %>%
-    mutate(answer.n = as.numeric(answer.id) + 1) %>%
-    select(-c(answer.data, answer.id))
+    mutate(answer.num = as.numeric(answer.id) + 1) %>%
+    select(attempt.id, question.id, answer.num, answer.time)
 
   ans_order = ans %>%
     filter_latest(answer.data = "_order") %>%
-    mutate(order.answer = count_commas(answer.id)) %>%
-    select(attempt.id, question.id, order.answer, answer.id,
+    mutate(answer.order = count_commas(answer.id)) %>%
+    select(attempt.id, question.id, answer.order, answer.id,
            order.time = answer.time)
 
-  ans_tidy =
-    left_join(ans_answer, ans_order,
-              by = c("attempt.id", "question.id")) %>%
+  ans_not_blank = ans_given %>%
+    left_join(ans_order, by = c("attempt.id", "question.id")) %>%
+    separate_rows(answer.id, answer.order, convert = TRUE) %>%
     group_by(attempt.id, question.id) %>%
-    filter(answer.time > order.time) %>%
-    separate_rows(answer.id, order.answer, convert = TRUE) %>%
-    filter(answer.n == order.answer) %>%
-    select(course.id, attempt.id, question.id, question.type,
-           question.maxpoints.past, answer.id, answer.time) %>%
-    as.data.frame()
+    filter(answer.time > order.time, answer.num == answer.order) %>%
+    select(attempt.id, question.id, answer.id, answer.time)
 
-  list(key = key, ans = ans_tidy, ans_raw = ans)
+  # Include blank answers
+  key_expanded = expand_key(
+    key = key,
+    attempt.id = unique(ans$attempt.id),
+    include.cols = "question.id")
+
+  ans_tidy = key_expanded %>%
+    left_join(ans_not_blank)
+
+  # Key tidy
+  key_tidy = key %>%
+    mutate(answer.correct = as.numeric(answer.percent > 0)) %>%
+    select(question.id, answer.id, question.text, answer.text,
+           answer.correct)
+
+  list(key = key_tidy, ans = ans_tidy)
 }
 
 #' Get multichocie (multiple answers) item data
