5d98c48fdc643c131b9c383f9bdefe0e95b0a96c
Jakub Chromec <chromec.jakub@gmail.com>
2017-03-31 22:10:25 +0200

    streamline, fetch distractors

--- R/s3_quiz.R
+++ R/s3_quiz.R
@@ -1,53 +1,3 @@
-#' Print moodle quiz
-#'
-#' Print mdl_quiz object.
-#' @param x Object of class \code{"mdl_quiz"}
-#' @param ... further arguments passed to methods
-#' @export
-
-print.mdl_quiz = function(x, ...) {
-
-  cat(
-    "\nQuiz: ", x$settings$quiz.name,
-    sep = "")
-
-  cat(
-    "\n\nquiz id:    ", x$settings$quiz.id,
-    " [course id: ", x$settings$course.id, "]",
-    "\nmodified:   ", as.character(x$settings$quiztimemodified),
-    sep = "")
-
-  cat(
-    "\nmax points: ", x$settings$sumgrades,
-    "\nmax grade:  ", x$settings$grade,
-    sep = "")
-
-  if (!is.null(x$attempts)) {
-    mean_min = mean(x$attempts$attempt.time.taken, na.rm = TRUE)
-    cat(
-      "\nattempts:   ",
-      x$settings$attemptscount,
-      " [", length(unique(x$attempts$user.id)),
-      " users, mean duration ", round(mean_min, 1), " min]",
-      sep = ""
-    )
-  } else {
-    cat("\nattempts:   0")
-  }
-
-  cat(
-    "\n\n",
-    length(unique(x$questions$question.id)), " items on ",
-    max(x$questions$page.number), " pages:\n\n",  sep = ""
-  )
-
-  item_count = structure(
-    as.data.frame(table(x$questions$question.type)),
-    names = c("item.type", "count"))
-
-  print(item_count, ...)
-}
-
 #' Get Quiz Data
 #'
 #' Fetch item data from quiz.
@@ -61,83 +11,136 @@ print.mdl_quiz = function(x, ...) {
 
 get_module_data.mdl_quiz = function(x, attempt = "first",
                                     question.type = NULL,
+                                    distractors = TRUE,
                                     prefix = "mdl_",
                                     suppress.warnings = TRUE,
                                     ...) {
 
   if (!nrow(x$attempts)) {
-    message("No attempts: aborted")
+    warning("No attempts: aborted", call. = FALSE)
     return()
   }
 
-  stopifnot(any(is.character(attempt), is.numeric(attempt)))
-
-  if (is.character(attempt)) {
-    attempt_id = get_attempt_id(
-      x = x$attempts,
-      attempt = attempt)
-  }
-
-  if (is.numeric(attempt)) {
-    attempt_id = dplyr::intersect(
-      x = attempt,
-      y = x$attempts$attempt.id)
-    if (length(attempt_id) > 0) {
-      message("Fetching attempts:\n", paste(attempt_id, collapse = " "))
-    } else {
-      stop("No such attempt", call. = FALSE)
-    }
-  }
+  available_attempts = get_attempt_id(
+    x = x$attempts,
+    attempt = attempt)
 
   if (is.null(question.type)) {
-    question_type = unique(x$questions$question.type)
+    available_questions = unique(x$questions$question.type)
+  } else if (!all(question.type %in% x$questions$question.type)) {
+    not_present = dplyr::setdiff(question.type, x$questions$question.type)
+    stop(not_present, " are not present in this quiz", call. = FALSE)
   } else {
-    question_type = dplyr::intersect(
-      x = question.type,
-      y = x$questions$question.type)
-    if (length(question_type) > 0) {
-      message(
-        "Fetching question types:\n",
-        paste(question_type, collapse = " "))
-    } else {
-      stop("No such question", call. = FALSE)
-    }
+    available_questions = question.type
   }
 
-  dat = lapply(
-    X = question_type,
+  # Fetch item-level data
+  items = get_question_ans(
+    conn = x$connection,
+    attempt.id = available_attempts,
+    question.type = "allquestions",
+    prefix = prefix,
+    suppress.warnings = suppress.warnings
+  )
+
+  items_tidy = items %>%
+    select(attempt.id, question.id, answer.time, answer.percent) %>%
+    left_join(x$questions, by = "question.id") %>%
+    select(attempt.id,
+           question.type, question.id, question.name, question.text,
+           page.number, slot.number,
+           answer.time, answer.percent)
+
+  if (!distractors)
+    return(
+      structure(
+        .Data = list(
+          settings = x$settings,
+          attempts = x$attempts,
+          items = items_tidy),
+        class = "mdl_quiz_data"
+      )
+    )
+
+  # Fetch distractor-level data
+  distractors = lapply(
+    X = available_questions,
     FUN = function(this_type) {
       this_fun = paste0("get_", this_type)
       if (exists(x = this_fun, mode = "function")) {
-        message("Fething: ", this_type)
         do.call(
           what = this_fun,
           args = list(
-            conn = x$connection,
-            quiz.id = x$settings$quiz.id,
-            attempt.id = attempt_id,
-            prefix = prefix,
-            suppress.warnings = suppress.warnings))
-      }
+            conn = x$connection, attempt.id = available_attempts,
+            prefix = prefix, suppress.warnings = suppress.warnings))}
     })
 
-  names(dat) = question_type
-  dat[!sapply(dat, is.null)]
+  names(distractors) = available_questions
+  distractors_tidy = distractors[!sapply(distractors, is.null)]
+
+  structure(
+    .Data = list(
+      settings = x$settings,
+      attempts = x$attempts,
+      items = items_tidy,
+      distractors = distractors_tidy),
+    class = "mdl_quiz_data"
+  )
 }
 
 get_attempt_id = function(x, attempt = "first") {
 
-  x = switch(
-    EXPR = attempt,
-    first = x %>%
-      group_by(user.id) %>%
-      filter(attempt.number == min(attempt.number)),
-    last =  x %>%
-      group_by(user.id) %>%
-      filter(attempt.number == max(attempt.number)),
-    all = x,
-    message("Invalid attempt specification; using 'all'")
-  )
+  stopifnot(any(
+    attempt %in% c("first", "last", "all"),
+    is.numeric(attempt)
+  ))
 
-  x$attempt.id
+  if (is.character(attempt)) {
+    x = switch(
+      EXPR = attempt,
+      first = x %>%
+        group_by(user.id) %>%
+        filter(attempt.number == min(attempt.number)),
+      last =  x %>%
+        group_by(user.id) %>%
+        filter(attempt.number == max(attempt.number)),
+      all = x,
+      stop("Invalid attempt specification (use one of 'first', 'last', 'all' or a numeric vector", call. = FALSE)
+    )
+    return(x$attempt.id)
+  }
+
+  if (length(dplyr::setdiff(attempt, x$attempt.id)) > 0) {
+    stop("These attempt IDs are not present: ",
+         dplyr::setdiff(attempt, x$attempt.id),
+         call. = FALSE)
+  } else {
+    return(attempt)
+  }
+}
+
+#' Extract key
+#'
+#' Extract key
+#' @param x Object of class \code{"mdl_quiz_data"}
+#' @param distractors List of distractors for to get key for; if \code{NULL} (the default) all keys will be extracted
+#' @export
+
+extract_key.mdl_quiz_data = function(x, distractors = NULL) {
+
+  if (is.null(distractors))
+    distractors = names(x$distractors)
+
+  all_keys = lapply(
+    X = distractors,
+    FUN = function(this_dist) {
+      key_num = x$distractors[[this_dist]]$key %>%
+        filter(answer.correct == 1) %>%
+        select(question.id, answer.num)
+      structure(
+        .Data = key_num$answer.num,
+        names = key_num$question.id
+      )})
+
+  unlist(all_keys)
 }
