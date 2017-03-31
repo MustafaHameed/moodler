21398c7aea44744cb0161301467600c0f2f745cc
Jakub Chromec <chromec.jakub@gmail.com>
2017-03-31 12:42:02 +0200

    standardize output

--- R/get_tf_sa.R
+++ R/get_tf_sa.R
@@ -2,13 +2,12 @@
 #'
 #' Get truefalse item data.
 #' @param conn Connection object
-#' @param quiz.id Numeric vector of length 1
 #' @param attempt.id Vector of attempt ids
 #' @param prefix Defaults to \code{"mdl_quiz"}
 #' @param suppress.warnings Should warnings produced by \code{\link[DBI]{dbGetQuery}} be suppressed? Defaults to \code{TRUE}
 #' @importFrom dplyr %>% left_join select mutate
 
-get_truefalse = function(conn, quiz.id, attempt.id, prefix = "mdl_",
+get_truefalse = function(conn, attempt.id, prefix = "mdl_",
                          suppress.warnings = TRUE) {
 
   # SQL queries
@@ -46,8 +45,8 @@ get_truefalse = function(conn, quiz.id, attempt.id, prefix = "mdl_",
 
   ans_tidy = ans_latest %>%
     mutate(answer.num = as.numeric(answer.text == "True")) %>%
-    right_join(key_expanded) %>%
-    left_join(key_num) %>%
+    right_join(key_expanded, by = c("attempt.id", "question.id")) %>%
+    left_join(key_num, by = c("question.id", "answer.num")) %>%
     select(attempt.id, question.id, answer.time, answer.num)
 
   list(key = key_tidy, ans = ans_tidy)
@@ -67,34 +66,50 @@ get_shortanswer = function(conn, quiz.id, attempt.id, prefix = "mdl_",
                            suppress.warnings = TRUE) {
 
   # SQL queries
-  key = get_question_key(
-    conn = conn,
-    question.type = "shortanswer",
-    quiz.id = quiz.id,
-    prefix = prefix)
-
   ans = get_question_ans(
     conn = conn,
     question.type = "shortanswer",
     attempt.id = attempt.id,
     prefix = prefix)
 
+  key = get_question_key(
+    conn = conn,
+    question.type = "shortanswer",
+    question.id = unique(ans$question.id),
+    prefix = prefix)
+
+  # Tidy key
+  key_tidy = key %>%
+    mutate(answer.num = "0,1",
+           answer.correct = answer.num) %>%
+    separate_rows(answer.num, answer.correct, convert = TRUE) %>%
+    mutate(answer.text = if_else(answer.num == 1, "Correct", "Incorrect")) %>%
+    select(question.id, question.text, question.type,
+           answer.text, answer.num, answer.correct) %>%
+    unique()
+
+  key_expanded = expand_key(
+    key = key,
+    attempt.id = unique(ans$attempt.id),
+    include.cols = "question.id"
+  )
+
+  key_num = select(key_tidy, question.id, answer.num)
+
+  # Tidy-up answers
   ans_answer = ans %>%
     filter_latest(answer.data = "answer") %>%
     select(-c(answer.percent, answer.data))
 
   ans_finish = ans %>%
     filter(!is.na(answer.percent)) %>%
-    rename(finish.time = answer.time) %>%
     group_by(attempt.id, question.id) %>%
-    filter(finish.time == max(finish.time)) %>%
-    select(attempt.id, question.id, answer.percent, finish.time)
+    filter(answer.time == max(answer.time))
 
-  ans_tidy = ans_answer %>%
-    left_join(ans_finish, by = c("attempt.id", "question.id")) %>%
-    filter(answer.time <= finish.time) %>%
-    mutate(answer.id = NA_real_) %>%
-    as.data.frame()
+  ans_tidy = ans_finish %>%
+    mutate(answer.num = as.numeric(answer.percent > 0)) %>%
+    select(attempt.id, question.id, answer.time, answer.num) %>%
+    right_join(key_expanded, by = c("attempt.id", "question.id"))
 
-  list(key = key, ans = ans_tidy, ans_raw = ans)
+  list(key = key_tidy, ans = ans_tidy)
 }
