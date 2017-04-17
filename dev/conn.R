dffbed9696be6c5ae65e024a9c3cf667ae13bad1
Jakub Chromec <chromec.jakub@gmail.com>
2017-04-17 20:14:58 +0200

    create new user, incl. local creds

--- dev/conn.R
+++ dev/conn.R
@@ -6,6 +6,18 @@ library(dplyr)
 library(moodler)
 
 # con ----
-.con = dbConnect(RMySQL::MySQL(), group = "moodler")
+.con = dbConnect(RMySQL::MySQL(),
+                 password = "moodler",
+                 username = "moodler",
+                 dbname = "moodle",
+                 host = "127.0.0.1")
+
 dbGetQuery(.con, "SET NAMES utf8")
 dbGetQuery(.con, "SET sql_mode = ''")
+
+# # create new user (if need be) ----
+# qry1 = "CREATE USER 'moodler'@'localhost' IDENTIFIED BY 'moodler';"
+# dbGetQuery(.con, qry1)
+#
+# qry2 = "GRANT ALL PRIVILEGES ON * . * TO 'moodler'@'localhost';"
+# dbGetQuery(.con, qry2)
