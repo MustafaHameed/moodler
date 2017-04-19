9c9e8ab5d39bcbc19095cbfbe355cd378929a7a5
juhy-git <libor.juhanak@gmail.com>
2017-04-19 15:51:53 +0200

    add forum get_settings

---
+++ inst/sql/forum/get_settings.sql
@@ -0,0 +1,31 @@
+SELECT
+
+-- id/name/type
+  f.course AS 'course.id',
+  cm.id AS 'forum.id',
+  f.name AS 'forum.name',
+  f.type AS 'forum.type',
+  
+-- discussions/posts counts 
+  (SELECT COUNT(fd.id)
+    FROM [prefix]forum_discussions AS fd
+    WHERE fd.forum = f.id) AS "discussion.count",
+  (SELECT COUNT(fp.id) FROM [prefix]forum_posts AS fp
+    JOIN [prefix]forum_discussions AS fd ON fd.id = fp.discussion
+    WHERE f.id = fd.forum) AS "post.count",
+
+-- grades and completion tracking
+  f.assessed AS 'forum.assessed',  # Is this an assessed forum (0 = no, 1+ = yes )
+  f.completionposts AS 'posts.required', # Certain number of posts is required to complete this activity.
+  f.completionreplies AS 'replies.required',
+  f.completiondiscussions AS 'discussions.required'
+
+-- forum modified
+	FROM_UNIXTIME(f.timemodified) AS 'forum.timemodified'
+  
+  
+FROM [prefix]forum AS f
+JOIN [prefix]course_modules AS cm
+  ON f.course = cm.course AND f.id = cm.instance
+
+WHERE cm.id IN ([module.id]);
