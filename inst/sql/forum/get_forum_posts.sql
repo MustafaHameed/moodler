9f5dbdccdcf12d945ba60870f3b75bd7238b3f23
Jakub Chromec <chromec.jakub@gmail.com>
2017-07-05 19:39:21 +0100

    add field

--- inst/sql/forum/get_forum_posts.sql
+++ inst/sql/forum/get_forum_posts.sql
@@ -5,9 +5,9 @@ SELECT
   cm.id AS 'forum.id',
   f.course AS 'course.id',
   f.type AS 'forum.type',
-  
+
 -- Discussion related
-  p.discussion AS 'discussion.id',  
+  p.discussion AS 'discussion.id',
 
 -- Post related
   p.id AS 'post.id',
@@ -20,51 +20,51 @@ SELECT
     THEN '0'
 	ELSE p.attachment
   END AS 'attachments.count',
-  
--- user related  
+
+-- user related
   p.userid AS 'user.id',
-  (SELECT CONCAT(u.lastname, ', ', u.firstname) FROM mdl_user AS u
+  (SELECT CONCAT(u.lastname, ', ', u.firstname) FROM [prefix]user AS u
     WHERE p.userid = u.id) AS 'user.name',
-  (SELECT r.shortname FROM mdl_role_assignments AS ra
-    JOIN mdl_context AS c ON ra.contextid = c.id
-    JOIN mdl_role AS r ON ra.roleid = r.id
-    WHERE ra.userid = p.userid AND c.instanceid = f.course AND 
-          r.id = 3 # = editingteacher 
+  (SELECT r.shortname FROM [prefix]role_assignments AS ra
+    JOIN [prefix]context AS c ON ra.contextid = c.id
+    JOIN [prefix]role AS r ON ra.roleid = r.id
+    WHERE ra.userid = p.userid AND c.instanceid = f.course AND
+          r.id = 3 # = editingteacher
   ) AS 'role.editteacher',
-  (SELECT r.shortname FROM mdl_role_assignments AS ra
-    JOIN mdl_context AS c ON ra.contextid = c.id
-    JOIN mdl_role AS r ON ra.roleid = r.id
-    WHERE ra.userid = p.userid AND c.instanceid = f.course AND 
-          r.id = 4 # = teacher 
+  (SELECT r.shortname FROM [prefix]role_assignments AS ra
+    JOIN [prefix]context AS c ON ra.contextid = c.id
+    JOIN [prefix]role AS r ON ra.roleid = r.id
+    WHERE ra.userid = p.userid AND c.instanceid = f.course AND
+          r.id = 4 # = teacher
   ) AS 'role.teacher',
-    (SELECT r.shortname FROM mdl_role_assignments AS ra
-    JOIN mdl_context AS c ON ra.contextid = c.id
-    JOIN mdl_role AS r ON ra.roleid = r.id
-    WHERE ra.userid = p.userid AND c.instanceid = f.course AND 
-          r.id = 5 # = student 
+    (SELECT r.shortname FROM [prefix]role_assignments AS ra
+    JOIN [prefix]context AS c ON ra.contextid = c.id
+    JOIN [prefix]role AS r ON ra.roleid = r.id
+    WHERE ra.userid = p.userid AND c.instanceid = f.course AND
+          r.id = 5 # = student
   ) AS 'role.student',
-    (SELECT r.shortname FROM mdl_role_assignments AS ra
-    JOIN mdl_context AS c ON ra.contextid = c.id
-    JOIN mdl_role AS r ON ra.roleid = r.id
-    WHERE ra.userid = p.userid AND c.instanceid = f.course AND 
-          r.id = 10 # id další role, kterou chci sledovat (z důvodu možnosti specifikování nestandardních rolí) 
+    (SELECT r.shortname FROM [prefix]role_assignments AS ra
+    JOIN [prefix]context AS c ON ra.contextid = c.id
+    JOIN [prefix]role AS r ON ra.roleid = r.id
+    WHERE ra.userid = p.userid AND c.instanceid = f.course AND
+          r.id = 10 # id další role, kterou chci sledovat (z důvodu možnosti specifikování nestandardních rolí)
   ) AS 'role.other1',
-    (SELECT r.shortname FROM mdl_role_assignments AS ra
-    JOIN mdl_context AS c ON ra.contextid = c.id
-    JOIN mdl_role AS r ON ra.roleid = r.id
-    WHERE ra.userid = p.userid AND c.instanceid = f.course AND 
-          r.id = 1 # id další role, kterou chci sledovat 
+    (SELECT r.shortname FROM [prefix]role_assignments AS ra
+    JOIN [prefix]context AS c ON ra.contextid = c.id
+    JOIN [prefix]role AS r ON ra.roleid = r.id
+    WHERE ra.userid = p.userid AND c.instanceid = f.course AND
+          r.id = 1 # id další role, kterou chci sledovat
   ) AS 'role.other2'
     # Zatím zřejmě jen provizorní řešení. Je třeba ještě podrobněji otestovat.
     # Momentálně v některých případech vyhazuje NULL. je třeba ověřit, jestli je to skutečně jen v případech,
     # kdy uživatel v kurzu nikdy nebyl zapsán/neměl přidělenou roli (spíše to vypadá, že ne).
-  
-FROM mdl_forum AS f
-JOIN mdl_course_modules AS cm
-  ON f.course = cm.course AND f.id = cm.instance
-JOIN mdl_forum_discussions AS d ON f.id= d.forum
-JOIN mdl_forum_posts AS p ON d.id = p.discussion
 
-WHERE cm.id IN (326485)
+FROM [prefix]forum AS f
+JOIN [prefix]course_modules AS cm
+  ON f.course = cm.course AND f.id = cm.instance
+JOIN [prefix]forum_discussions AS d ON f.id= d.forum
+JOIN [prefix]forum_posts AS p ON d.id = p.discussion
+
+WHERE cm.id IN ([forum.id])
 
 ORDER BY f.course, f.id, f.type, p.discussion;
